17185522f6c78a11b057d61a91f3c4fee19e962e
G Locke <jkeys726@gmail.com>
2024-04-21 17:32:38 -0500

    Refactor course class to use enum for term and update cs-major script

--- php-scripts/cs-major.php
+++ php-scripts/cs-major.php
@@ -11,12 +11,11 @@
     $cs_courses = fetchCourses($sql, $connect);
 
     foreach ($cs_courses as $key => $value) {
-        $course = new Course();
-        $course->parse_sql_arry($value);
-        if ($course->offered_main_campus_fall) {
+        $course = new Course($value);
+        if ($course->offered_main_campus_term == Term::FALL || $course->offered_main_campus_term == Term::BOTH) {
             $fall_courses[$course->course_code] = $course;
         }
-        if ($course->offered_main_campus_spring) {
+        if ($course->offered_main_campus_term == Term::SPRING || $course->offered_main_campus_term == Term::BOTH) {
             $spring_courses[$course->course_code] = $course;
         }
         if ($course->offered_main_campus_even_year) {
@@ -29,6 +28,49 @@
         $course->printCourse();
         echo "---------------------------------------------<br>";
     }
-    
-    // $freshman_fall = new Semester(1, Term::FALL);
+
+    // create the statistics option:
+    $business_stats = fetchSingleCourse("BUS-2193", $connect);
+    $prob_and_stat = fetchSingleCourse("MTH-3183", $connect);
+    $applied_stat = fetchSingleCourse("MTH-2103", $connect);
+    $stats_for_behavioral = fetchSingleCourse("PSY-2383", $connect);
+
+    $stats_option = new CourseOption(3);
+    $stats_option->addOption($business_stats, 1);
+    $stats_option->addOption($prob_and_stat, 0);
+    $stats_option->addOption($applied_stat, 0);
+    $stats_option->addOption($stats_for_behavioral, 0);
+
+    $requirements = array(
+        "CS-1233",
+        "CS-1513",
+        "CS-2173",
+        "CS-2243",
+        "CS-2423",
+        "CS-2823",
+        "CS-3363",
+        "CS-3533",
+        "MTH-1153",
+        "MTH-1163",
+        "MTH-2213",
+        $stats_option,
+        "CS-3213",
+        "EE-2211",
+        "EE-2212",
+        "EN-1112",
+        "EN-3222",
+        "EN-4113",
+        "EN-4123"        
+    );
+
+    $freshman_fall = new Semester(1, Term::FALL);
+    // foreach ($object in $requirements) {
+
+    // }
+
+
+    // echo "<pre>";
+    // print_r($stats_option);
+    // echo "</pre>";
+
 ?>
