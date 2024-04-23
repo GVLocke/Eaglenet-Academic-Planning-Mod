8932c9097d4ed5e7c468eea240a2cdebbc797af2
G Locke <jkeys726@gmail.com>
2024-04-22 23:59:57 -0500

    fixed some issues, going to start work on the nesting problem

--- php-scripts/parse-Expression.php
+++ php-scripts/parse-Expression.php
@@ -1,30 +1,41 @@
 <?php
     function parseExpression($str) {
-            $expression_array = array();
-            $count = 0;
-            while (strpos($str, "(") !== false && strpos($str, ")") !== false) {
-                $str = preg_replace_callback('/\((.*?)\)/', function($matches) use (&$count, &$expression_array) {
-                    $expression_array[] = parseInnerExpression($matches[1]);
-                    // print_r($expression_array);
-                    return $count++;
-                }, $str);
-            }
-            $operators = ['and', 'or'];
-            $top_op = "";
-            foreach ($operators as $op) {
-                if (strpos($str, $op) !== false) {
-                    $top_op = $op;
-                    break;
-                }
-            }
-            return [
-                "operator" => $top_op,
-                "terms" => $expression_array
-            ];
+        if ($str == "") {
+            return "";
         }
+        elseif (strpos($str, "(") === false && strpos($str, ")") === false) {
+            return parseInnerExpression($str);
+        }
+        else {
+            return parseExpressionMain($str);
+        }
+    }
+
+    function parseExpressionMain($str) {
+        $expression_array = array();
+        $count = 0;
+        while (strpos($str, "(") !== false && strpos($str, ")") !== false) {
+            $str = preg_replace_callback('/\((.*?)\)/', function($matches) use (&$count, &$expression_array) {
+                $expression_array[] = parseInnerExpression($matches[1]);
+                // print_r($expression_array);
+                return $count++;
+            }, $str);
+        }
+        $operators = ['and', 'or'];
+        $top_op = "";
+        foreach ($operators as $op) {
+            if (strpos($str, $op) !== false) {
+                $top_op = $op;
+                break;
+            }
+        }
+        return [
+            "operator" => $top_op,
+            "terms" => $expression_array
+        ];
+    }
 
     function parseInnerExpression($str) {
-        if (strpos($str, "(") == false && strpos($str, ")") == false) {
             $operators = ['and', 'or'];
             foreach ($operators as $op) {
                 if (strpos($str, $op) !== false) { // there's an operator inside
@@ -39,6 +50,5 @@
                 "operator" => 'none',
                 "terms" => $str
             ];
-        }
     }
 ?>
