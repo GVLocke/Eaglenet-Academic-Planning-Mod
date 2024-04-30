15841da1230f3026a791e916756b98ca9ae94e24
G Locke <jkeys726@gmail.com>
2024-04-30 08:19:50 -0500

    removed unused javascript

--- js/angular_scripts.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('coursesCtrl', function($scope, $http) {
-   $http.get("./php-scripts/getCS-courses.php")
-   .then(function (response) {
-    $scope.courses = response.data;
-  });
-});
--- php-scripts/AcademicPlanningMod/CourseOption.php
+++ php-scripts/AcademicPlanningMod/CourseOption.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace AcademicPlanningMod;
 
 use InvalidArgumentException;
